2dde25d3651fe3ea17a9a9bf870a751dd6daf9c3
DESKTOP-MCJ6N1U\bogat <bogataj.patrik@gmail.com>
2020-11-15 23:35:16 +0100

    Dodal tabelo "User"

--- server/api/Models/User.cs
+++ server/api/Models/User.cs
@@ -1,16 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace api.Models
 {
     public class User
     {
-        int Id { get; set; } // polje id
-        string Username { get; set; } // polkje userame
-        string Password { get; set; }
-        DateTime CreatedAt { get; set; }
-        string FirstName { get; set; }
-        string LastName { get; set; }
-        DateTime BirthDate { get; set; }
-        string Description { get; set; }
+        [Key]
+        public int Id { get; set; } // polje id
+        public string Username { get; set; } // polkje userame
+        public string Password { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Description { get; set; }
     }
 }
