1ab5912cdb654aab8e1fd0e07793fd7e05a13a6c
DESKTOP-MCJ6N1U\bogat <bogataj.patrik@gmail.com>
2020-11-20 20:10:49 +0100

    Dopolnil kontroler (updateProperties, CreateAt, if stavki v UpdateProperties)

--- server/api/Controllers/PartyController.cs
+++ server/api/Controllers/PartyController.cs
@@ -50,7 +50,11 @@ namespace api.Controllers
                 return BadRequest();
             }
 
-            _context.Entry(party).State = EntityState.Modified;
+            var previousParty = await _context.Parties.FindAsync(id);
+
+            UpdateProperties(previousParty, party);
+
+            _context.Entry(previousParty).State = EntityState.Modified;
 
             try
             {
@@ -75,6 +79,8 @@ namespace api.Controllers
         [HttpPost]
         public async Task<ActionResult<Party>> PostParty(Party party)
         {
+            party.CreatedAt = DateTime.Now;
+
             _context.Parties.Add(party);
             await _context.SaveChangesAsync();
 
@@ -100,6 +106,20 @@ namespace api.Controllers
         private bool PartyExists(int id)
         {
             return _context.Parties.Any(e => e.Id == id);
+        }
+        
+        private void UpdateProperties(Party previousParty, Party updatedParty)
+        {
+            if (updatedParty.Title != null) previousParty.Title = updatedParty.Title;
+            if (updatedParty.Description != null) previousParty.Description = updatedParty.Description;
+            if (updatedParty.Date != null) previousParty.Date = updatedParty.Date;
+            if (updatedParty.Price != 0 && previousParty.Price != 0) previousParty.Price = updatedParty.Price;
+            if (updatedParty.Location != null) previousParty.Location = updatedParty.Location;
+            if (updatedParty.Capacity != 0 && previousParty.Capacity != 0) previousParty.Capacity = updatedParty.Capacity;
+            if (updatedParty.IsPublic) previousParty.IsPublic = updatedParty.IsPublic;
+            if (updatedParty.UserId != 0 && previousParty.UserId != 0) previousParty.UserId = updatedParty.UserId;
+            if (updatedParty.PartyStatusId != 0 && previousParty.PartyStatusId != 0) previousParty.PartyStatusId = updatedParty.PartyStatusId;
+            
         }
     }
 }
