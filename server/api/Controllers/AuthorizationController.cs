564818bf32c7f5e4ac20f399263424be0a4875cd
DESKTOP-MCJ6N1U\bogat <bogataj.patrik@gmail.com>
2020-11-25 20:15:10 +0100

    Dodal AuthorizationController

---
+++ server/api/Controllers/AuthorizationController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace api.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class AuthorizationController : ControllerBase
+    {
+        private readonly WePartyDBContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public AuthorizationController(
+            WePartyDBContext context,
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager
+        )
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<ApplicationUser>> Register(ApplicationUser newUser)
+        {
+            var user = new ApplicationUser()
+            {
+                UserName = newUser.UserName
+            };
+
+            var result = await _userManager.CreateAsync(user, newUser.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+            }
+            else
+            {
+                return BadRequest();
+            }
+
+            return newUser;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<ApplicationUser>> Login(ApplicationUser userCredentials)
+        {
+            var result = await _signInManager.PasswordSignInAsync(userCredentials.UserName, userCredentials.Password, false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
+            {
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
+
