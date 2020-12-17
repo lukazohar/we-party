c4eb795e61f11397a18666a185175acc14e7bf4d
Racunalnik\Patrik <bogataj.patrik@gmail.com>
2020-12-17 17:09:51 +0100

    prikaz random json podatkov

--- app/src/main/java/com/example/we_party/MainActivity.java
+++ app/src/main/java/com/example/we_party/MainActivity.java
@@ -7,71 +7,27 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextViewResult;
-    private RequestQueue mQueue;
-
+    Button click;
+    public static TextView data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextViewResult = findViewById(R.id.text_view_result);
-        Button buttonParse =findViewById(R.id.button_parse);
+        click = findViewById(R.id.button);
+        data = findViewById(R.id.fetcheddata);
 
-        mQueue = Volley.newRequestQueue(this);
-
-        buttonParse.setOnClickListener(new View.OnClickListener() {
+        click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                jsonParse();
+                fetchData process = new fetchData();
+                process.execute();
             }
         });
+
     }
 
-    private void jsonParse(){
-
-
-
-        String url="https://jsonplaceholder.typicode.com/todos";
-
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            JSONArray jsonArray = response.getJSONArray("data");
-                            for (int i = 0; i < jsonArray.length(); i++) {
-                                JSONObject data = ((JSONArray) jsonArray).getJSONObject(i);
-                                int userId = data.getInt("userId");
-                                int Id = data.getInt("Id");
-                                String title = data.getString("title");
-                                boolean completed = data.getBoolean("completed");
-                                mTextViewResult.append(title + ", " + String.valueOf(userId) + ", " + Id + "\n\n");
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-            }
-        });
-        mQueue.add(request);
-    }
 }
-
