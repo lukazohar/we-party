c5096a8b4a2d4d45bf5cffff1de9f671f1c164c0
Luka Žohar <luka.zohi@gmail.com>
2021-01-04 21:47:40 +0100

    Client: zbrisan modul tabs in zihtane route

--- client/we-party/src/app/app-routing.module.ts
+++ client/we-party/src/app/app-routing.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  /* 
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'parties',
+    loadChildren: () => import('./party/party.module').then(m => m.PartyPageModule)
+  },
+  {
+    path: 'applications',
+    loadChildren: () => import('./application/application.module').then(m => m.ApplicationPageModule)
+  },
+  {
+    path: 'friendships',
+    loadChildren: () => import('./friendship/friendship.module').then(m => m.FriendshipPageModule)
+  }, */
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
