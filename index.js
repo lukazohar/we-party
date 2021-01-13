82a11ed5ebe2d30bd7007938d8c20301d2276f4d
Luka Žohar <luka.zohi@gmail.com>
2021-01-13 09:58:00 +0100

    Vkljopljen CORS pri frontendu

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const app = express();
 
 app.use(express.static('./www/'));
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json',
+  );
+  next();
+});
+
 const port = process.env.PORT || 3333;
 app.listen(port, () =>
   console.log(`we-party listening at http://localhost:${port}`),
